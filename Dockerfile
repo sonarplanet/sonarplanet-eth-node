fe60b2a0025683e70f45909666f5d13e37ba1e74
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-04-19 16:56:31 +0200

    build: switch ws port to 8080

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM parity/parity
 COPY ./light_fast_config.toml . 
 COPY ./startup.sh .
 
-EXPOSE 8546
+EXPOSE 8080
 
 ENTRYPOINT [ "/bin/bash", "./startup.sh" ]
--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/parity/parity --config light_fast_config.toml --geth --light &
+/parity/parity --config light_fast_config.toml --geth --light 
 
-echo "-- START PYTHON HTTP SERVER ON 8080"
+#echo "-- START PYTHON HTTP SERVER ON 8080"
 
-python3 -m http.server 8080 &> ws.log
+#python3 -m http.server 8080 &> ws.log
